c71ee3066c7dcab345688af89e8a494db4e7aae2
Anatoly <abrekov@ya.ru>
2019-11-10 14:07:41 +0300

    add task12

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Solution.print(1);
+        Solution.print(Integer.valueOf(2));
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int n) {
+
+    }
+
+    public static void print(Integer n) {
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(String s) {}
+    public static void print(String s, int n) {}
+    public static void print(String s, String s1) {}
+    public static void print(String s, String s1, String s2) {}
+    public static void print(String s, String s1, String s2, String s3) {}
+
+}
